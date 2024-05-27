f99c881948731b84a310d8de43601d2f089a6018
Carlos Eduardo dos Santos <carloskaduzinho2013@hotmail.com>
2024-05-27 20:51:57 -0300

    criei ex10,11,12,13

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,8 @@
+console.log('o console funcionou perfeitamente!')
+
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) { // Condição simples!
+    console.log('você ultrapassou a velocidade minima permitida. MULTADO')
+}
+console.log('Dirija sempre usando cinto de segurança')
