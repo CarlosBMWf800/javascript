bc895a51b2f09d023c06f6a5e9b337d141c90525
Carlos Eduardo dos Santos <carloskaduzinho2013@hotmail.com>
2024-06-04 20:23:59 -0300

    ex 014

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1
+while (c < 6) {
+    console.log('tudo bem?')
+    c++
+}
+
+
+
+
+/* while e do são estruturas de repetições */
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
