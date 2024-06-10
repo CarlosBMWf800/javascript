2c8ba7b51899cd62fffe78dcdf29e32cf2f04fe8
Carlos Eduardo dos Santos <carloskaduzinho2013@hotmail.com>
2024-06-09 21:43:11 -0300

    criei ex015

---
+++ exercicios/aula15/ambiente.js
@@ -0,0 +1,13 @@
+let num = [5,8,2,9,3]
+
+num.sort()
+console.log(num)
+console.log(`Nosso vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(8)
+console.log(`O valor está na posição ${pos}`)
+/* num.push(7) adiciona uma array na ultima posição
+   num.length atributo que mostra quantidade de elementos no vetor
+   num.sort() coloca todos os elementos em ordem crescente
+   vet ou var pode ser usado para adicionar varíaveis
+*/
